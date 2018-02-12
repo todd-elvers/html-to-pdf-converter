7244dd6d7a12472b5411141c0c7f9181cfa204b5
Todd Elvers <todd.elvers@gmail.com>
2018-02-11 21:41:39 -0600

    Adding fix for failing tests on Windows

--- src/test/groovy/te/htmltopdf/pdf/HtmlToPdfBinaryResolverTest.groovy
+++ src/test/groovy/te/htmltopdf/pdf/HtmlToPdfBinaryResolverTest.groovy
@@ -1,5 +1,6 @@
 package te.htmltopdf.pdf
 
+import spock.lang.IgnoreIf
 import te.htmltopdf.HtmlToPdfBinaryResolver
 import te.htmltopdf.testHelpers.ResourceFinding
 import org.apache.commons.lang3.SystemUtils
@@ -16,16 +17,27 @@ class HtmlToPdfBinaryResolverTest extends Specification implements ResourceFindi
     @Rule
     TemporaryFolder temporaryFolder = new TemporaryFolder()
 
+    @IgnoreIf({ isOsWindows() })
     void "always ensures the binary it is instantiated with has the executable flag"() {
         given: 'a webkitHtmlToPdf binary missing the executable flag'
             File wkHtmlToPdfBinary = getOsSpecificBinary()
-            assert wkHtmlToPdfBinary.setExecutable(false) : "Failed to set the file to NOT executable first."
+            wkHtmlToPdfBinary.setExecutable(false)
+            assert !Files.isExecutable(wkHtmlToPdfBinary.toPath())
 
         when: 'webkitHtmlToPdf binary bean is instantiated'
             def webkitHtmlToPdfExecutable = binaryResolver.resolve()
 
         then: 'the executable flag is set'
             Files.isExecutable(webkitHtmlToPdfExecutable.toPath())
+    }
+
+    @IgnoreIf({ isOsWindows() })
+    void "throws RuntimeException if the wkhtmltopdf's executable flag cannot be set"() {
+        when:
+            binaryResolver.asExecutable(new File('not-going-to-find-this'))
+
+        then:
+            thrown(RuntimeException)
     }
 
     void "will use binary from environment variable over binary from JAR"() {
@@ -83,4 +95,7 @@ class HtmlToPdfBinaryResolverTest extends Specification implements ResourceFindi
         }
     }
 
+    private static boolean isOsWindows() {
+        return System.properties['os.name']?.toString()?.toLowerCase()?.contains("windows")
+    }
 }
