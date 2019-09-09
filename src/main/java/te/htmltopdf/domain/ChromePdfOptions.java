55e1cefa8688e6ee431048d8b3892a43db0336c7
todd-elvers <todd.elvers@gmail.com>
2019-09-09 16:50:19 -0500

    Converted to immutable-builder pattern

--- src/main/java/te/htmltopdf/domain/ChromePdfOptions.java
+++ src/main/java/te/htmltopdf/domain/ChromePdfOptions.java
@@ -2,23 +2,23 @@ package te.htmltopdf.domain;
 
 public class ChromePdfOptions {
 
-    private Boolean landscape;
-    private Boolean displayHeaderFooter;
-    private Boolean printBackground;
-    private Boolean ignoreInvalidPageRanges;
-    private Boolean preferCSSPageSize;
+    private final Boolean landscape;
+    private final Boolean displayHeaderFooter;
+    private final Boolean printBackground;
+    private final Boolean ignoreInvalidPageRanges;
+    private final Boolean preferCSSPageSize;
 
-    private Double scale;
-    private Double paperWidth;
-    private Double paperHeight;
-    private Double marginTop;
-    private Double marginBottom;
-    private Double marginLeft;
-    private Double marginRight;
+    private final Double scale;
+    private final Double paperWidth;
+    private final Double paperHeight;
+    private final Double marginTop;
+    private final Double marginBottom;
+    private final Double marginLeft;
+    private final Double marginRight;
 
-    private String pageRanges;
-    private String headerTemplate;
-    private String footerTemplate;
+    private final String pageRanges;
+    private final String headerTemplate;
+    private final String footerTemplate;
 
     private ChromePdfOptions(Boolean landscape, Boolean displayHeaderFooter, Boolean printBackground, Boolean ignoreInvalidPageRanges, Boolean preferCSSPageSize, Double scale, Double paperWidth, Double paperHeight, Double marginTop, Double marginBottom, Double marginLeft, Double marginRight, String pageRanges, String headerTemplate, String footerTemplate) {
         this.landscape = landscape;
@@ -42,135 +42,60 @@ public class ChromePdfOptions {
         return landscape;
     }
 
-    public ChromePdfOptions setLandscape(Boolean landscape) {
-        this.landscape = landscape;
-        return this;
-    }
-
     public Boolean getDisplayHeaderFooter() {
         return displayHeaderFooter;
-    }
-
-    public ChromePdfOptions setDisplayHeaderFooter(Boolean displayHeaderFooter) {
-        this.displayHeaderFooter = displayHeaderFooter;
-        return this;
     }
 
     public Boolean getPrintBackground() {
         return printBackground;
     }
 
-    public ChromePdfOptions setPrintBackground(Boolean printBackground) {
-        this.printBackground = printBackground;
-        return this;
-    }
-
     public Boolean getIgnoreInvalidPageRanges() {
         return ignoreInvalidPageRanges;
-    }
-
-    public ChromePdfOptions setIgnoreInvalidPageRanges(Boolean ignoreInvalidPageRanges) {
-        this.ignoreInvalidPageRanges = ignoreInvalidPageRanges;
-        return this;
     }
 
     public Boolean getPreferCSSPageSize() {
         return preferCSSPageSize;
     }
 
-    public ChromePdfOptions setPreferCSSPageSize(Boolean preferCSSPageSize) {
-        this.preferCSSPageSize = preferCSSPageSize;
-        return this;
-    }
-
     public Double getScale() {
         return scale;
-    }
-
-    public ChromePdfOptions setScale(Double scale) {
-        this.scale = scale;
-        return this;
     }
 
     public Double getPaperWidth() {
         return paperWidth;
     }
 
-    public ChromePdfOptions setPaperWidth(Double paperWidth) {
-        this.paperWidth = paperWidth;
-        return this;
-    }
-
     public Double getPaperHeight() {
         return paperHeight;
-    }
-
-    public ChromePdfOptions setPaperHeight(Double paperHeight) {
-        this.paperHeight = paperHeight;
-        return this;
     }
 
     public Double getMarginTop() {
         return marginTop;
     }
 
-    public ChromePdfOptions setMarginTop(Double marginTop) {
-        this.marginTop = marginTop;
-        return this;
-    }
-
     public Double getMarginBottom() {
         return marginBottom;
-    }
-
-    public ChromePdfOptions setMarginBottom(Double marginBottom) {
-        this.marginBottom = marginBottom;
-        return this;
     }
 
     public Double getMarginLeft() {
         return marginLeft;
     }
 
-    public ChromePdfOptions setMarginLeft(Double marginLeft) {
-        this.marginLeft = marginLeft;
-        return this;
-    }
-
     public Double getMarginRight() {
         return marginRight;
-    }
-
-    public ChromePdfOptions setMarginRight(Double marginRight) {
-        this.marginRight = marginRight;
-        return this;
     }
 
     public String getPageRanges() {
         return pageRanges;
     }
 
-    public ChromePdfOptions setPageRanges(String pageRanges) {
-        this.pageRanges = pageRanges;
-        return this;
-    }
-
     public String getHeaderTemplate() {
         return headerTemplate;
     }
 
-    public ChromePdfOptions setHeaderTemplate(String headerTemplate) {
-        this.headerTemplate = headerTemplate;
-        return this;
-    }
-
     public String getFooterTemplate() {
         return footerTemplate;
-    }
-
-    public ChromePdfOptions setFooterTemplate(String footerTemplate) {
-        this.footerTemplate = footerTemplate;
-        return this;
     }
 
     public static class Builder {
@@ -266,7 +191,23 @@ public class ChromePdfOptions {
         }
 
         public ChromePdfOptions build() {
-            return new ChromePdfOptions(landscape, displayHeaderFooter, printBackground, ignoreInvalidPageRanges, preferCSSPageSize, scale, paperWidth, paperHeight, marginTop, marginBottom, marginLeft, marginRight, pageRanges, headerTemplate, footerTemplate);
+            return new ChromePdfOptions(
+                    landscape,
+                    displayHeaderFooter,
+                    printBackground,
+                    ignoreInvalidPageRanges,
+                    preferCSSPageSize,
+                    scale,
+                    paperWidth,
+                    paperHeight,
+                    marginTop,
+                    marginBottom,
+                    marginLeft,
+                    marginRight,
+                    pageRanges,
+                    headerTemplate,
+                    footerTemplate
+            );
         }
     }
 }
