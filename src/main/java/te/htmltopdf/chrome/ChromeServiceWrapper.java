2c9fcd45405e072daece23faa2a8b2e0077fde0c
todd-elvers <todd.elvers@gmail.com>
2019-09-09 00:16:38 -0500

    Converted this Groovy file to Java

--- src/main/java/te/htmltopdf/chrome/ChromeServiceWrapper.java
+++ src/main/java/te/htmltopdf/chrome/ChromeServiceWrapper.java
@@ -1,48 +1,46 @@
-//package te.htmltopdf.chrome;
-//
-//import com.github.kklisura.cdt.launch.ChromeLauncher
-//import com.github.kklisura.cdt.services.ChromeDevToolsService
-//import com.github.kklisura.cdt.services.ChromeService
-//import com.github.kklisura.cdt.services.types.ChromeTab
-//import groovy.transform.CompileStatic
-//import org.springframework.stereotype.Component
-//
-//@CompileStatic
-//@Component
-//class ChromeServiceWrapper {
-//
-//    private ChromeService chromeService
-//    private ChromeLauncher chromeLauncher
-//
-//    private ChromeLauncher getChromeLauncher() {
-//        if (!isChromeAlive()) {
-//            chromeLauncher = new ChromeLauncher()
-//        }
-//        return chromeLauncher
-//    }
-//
-//    private ChromeService getChromeService() {
-//        if (!isChromeAlive() || !chromeService) {
-//            chromeService = getChromeLauncher().launch(true)
-//        }
-//        return chromeService
-//    }
-//
-//    boolean isChromeAlive() {
-//        return chromeLauncher && chromeLauncher.isAlive()
-//    }
-//
-//    void closeTab(ChromeTab tab) {
-//        if (tab) {
-//            chromeService.closeTab(tab)
-//        }
-//    }
-//
-//    ChromeTab getChromeTab() {
-//        return getChromeService().createTab()
-//    }
-//
-//    ChromeDevToolsService getChromeDevToolsService(ChromeTab tab) {
-//        return getChromeService().createDevToolsService(tab)
-//    }
-//}
+package te.htmltopdf.chrome;
+
+import com.github.kklisura.cdt.launch.ChromeLauncher;
+import com.github.kklisura.cdt.services.ChromeDevToolsService;
+import com.github.kklisura.cdt.services.ChromeService;
+import com.github.kklisura.cdt.services.types.ChromeTab;
+
+class ChromeServiceWrapper {
+
+    private ChromeService chromeService;
+    private ChromeLauncher chromeLauncher;
+
+    private ChromeLauncher getChromeLauncher() {
+        if (!isChromeAlive()) {
+            chromeLauncher = new ChromeLauncher();
+        }
+
+        return chromeLauncher;
+    }
+
+    private ChromeService getChromeService() {
+        if (!isChromeAlive() || chromeService != null) {
+            chromeService = getChromeLauncher().launch(true);
+        }
+
+        return chromeService;
+    }
+
+    boolean isChromeAlive() {
+        return chromeLauncher != null && chromeLauncher.isAlive();
+    }
+
+    void closeTab(ChromeTab tab) {
+        if (tab != null) {
+            chromeService.closeTab(tab);
+        }
+    }
+
+    ChromeTab getChromeTab() {
+        return getChromeService().createTab();
+    }
+
+    ChromeDevToolsService getChromeDevToolsService(ChromeTab tab) {
+        return getChromeService().createDevToolsService(tab);
+    }
+}
