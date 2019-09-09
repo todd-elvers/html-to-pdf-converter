4314915b45fe1b090b1ee6713cf60eae095b6e74
todd-elvers <todd.elvers@gmail.com>
2019-09-09 00:42:52 -0500

    Converted this Groovy file to Java

--- src/main/java/te/htmltopdf/chrome/ChromePdfFileGenerator.java
+++ src/main/java/te/htmltopdf/chrome/ChromePdfFileGenerator.java
@@ -1,78 +1,89 @@
-//package te.htmltopdf.chrome;
-//
-//
-//import com.github.kklisura.cdt.protocol.commands.Page
-//import com.github.kklisura.cdt.protocol.events.page.LoadEventFired
-//import com.github.kklisura.cdt.protocol.support.types.EventHandler
-//import com.github.kklisura.cdt.services.ChromeDevToolsService
-//import com.github.kklisura.cdt.services.types.ChromeTab
-//import groovy.transform.CompileStatic
-//import groovy.util.logging.Slf4j
-//import org.springframework.beans.factory.annotation.Autowired
-//import org.springframework.stereotype.Component
-//
-//@Slf4j
-//@SuppressWarnings("GrMethodMayBeStatic")
-//@CompileStatic
-//@Component
-//class ChromePdfFileGenerator {
-//
-//    @Autowired
-//    ChromeServiceWrapper chromeServiceWrapper
-//
-//    PdfFile generate(ChromePdfOptions options, File htmlFile) {
-//        ChromeTab chromeTab = null
-//        ChromeDevToolsService devToolsService = null
-//
-//        File rawPdfFile = File.createTempFile("report", ".pdf")
-//        try {
-//            chromeTab = chromeServiceWrapper.getChromeTab()
-//            devToolsService = chromeServiceWrapper.getChromeDevToolsService(chromeTab)
-//            devToolsService.emulation.emulatedMedia = 'print'
-//
-//            Page page = devToolsService.getPage()
-//            page.enable()
-//            page.navigate('file://' + htmlFile.absolutePath)
-//            page.onLoadEventFired(new EventHandler<LoadEventFired>() {
-//                @Override
-//                void onEvent(LoadEventFired event) {
-//                    String pdfFileContents = printPdfToString(page, options)
-//                    writeContentsToFile(pdfFileContents, rawPdfFile)
-//                    devToolsService?.close()
-//                }
-//            })
-//        } finally {
-//            devToolsService?.waitUntilClosed()
-//            chromeServiceWrapper.closeTab(chromeTab)
-//            htmlFile.delete()
-//        }
-//        log.debug("PDF generation complete.")
-//        return new PdfFile(rawPdfFile)
-//    }
-//
-//    protected String printPdfToString(Page page, ChromePdfOptions options) {
-//        return page.printToPDF(
-//                options.landscape,
-//                options.displayHeaderFooter,
-//                options.printBackground,
-//                options.scale,
-//                options.paperWidth,
-//                options.paperHeight,
-//                options.marginTop,
-//                options.marginBottom,
-//                options.marginLeft,
-//                options.marginRight,
-//                options.pageRanges,
-//                options.ignoreInvalidPageRanges,
-//                options.headerTemplate,
-//                options.footerTemplate,
-//                options.preferCSSPageSize
-//        )
-//    }
-//
-//    protected void writeContentsToFile(String data, File file) {
-//        file.withOutputStream { outputStream ->
-//                outputStream.write(Base64.decoder.decode(data))
-//        }
-//    }
-//}
+package te.htmltopdf.chrome;
+
+
+import com.github.kklisura.cdt.protocol.commands.Page;
+import com.github.kklisura.cdt.services.ChromeDevToolsService;
+import com.github.kklisura.cdt.services.types.ChromeTab;
+import io.vavr.control.Try;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import te.htmltopdf.TempFileGenerator;
+import te.htmltopdf.domain.PdfFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Base64;
+
+class ChromePdfFileGenerator {
+    private static final Logger log = LoggerFactory.getLogger(ChromePdfFileGenerator.class);
+
+    private ChromeServiceWrapper chromeServiceWrapper;
+    private TempFileGenerator tempFileGenerator;    // TODO: add use case for the temp file generation here
+
+    PdfFile generate(ChromePdfOptions options, File htmlFile) {
+        ChromeTab chromeTab = chromeServiceWrapper.getChromeTab();
+        ChromeDevToolsService devToolsService = chromeServiceWrapper.getChromeDevToolsService(chromeTab);
+
+        File rawPdfFile = Try
+                .of(() -> File.createTempFile("report", ".pdf"))
+                .get();
+
+        try {
+//            chromeTab = chromeServiceWrapper.getChromeTab();
+//            devToolsService = chromeServiceWrapper.getChromeDevToolsService(chromeTab);
+            devToolsService.getEmulation().setEmulatedMedia("print");
+
+            Page page = devToolsService.getPage();
+            page.enable();
+            page.navigate("file://" + htmlFile.getAbsolutePath());
+            page.onLoadEventFired(event -> {
+                String pdfFileContents = printPdfToString(page, options);
+                try {
+                    writeContentsToFile(pdfFileContents, rawPdfFile);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                devToolsService.close();
+            });
+        } catch (Exception e) {
+            log.error("Failed to write PDF contents to file.");
+        } finally {
+            if (devToolsService != null) {
+                devToolsService.waitUntilClosed();
+            }
+            chromeServiceWrapper.closeTab(chromeTab);
+            htmlFile.delete();
+        }
+        log.debug("PDF generation complete.");
+        return new PdfFile(rawPdfFile);
+    }
+
+    protected String printPdfToString(Page page, ChromePdfOptions options) {
+        return page.printToPDF(
+                options.getLandscape(),
+                options.getDisplayHeaderFooter(),
+                options.getPrintBackground(),
+                options.getScale(),
+                options.getPaperWidth(),
+                options.getPaperHeight(),
+                options.getMarginTop(),
+                options.getMarginBottom(),
+                options.getMarginLeft(),
+                options.getMarginRight(),
+                options.getPageRanges(),
+                options.getIgnoreInvalidPageRanges(),
+                options.getHeaderTemplate(),
+                options.getFooterTemplate(),
+                options.getPreferCSSPageSize()
+        );
+    }
+
+    protected void writeContentsToFile(String data, File file) throws IOException {
+        try (OutputStream stream = Files.newOutputStream(file.toPath())) {
+            stream.write(Base64.getDecoder().decode(data));
+        }
+    }
+}
