80823bbcdc72c01f378972056d9d0bd746ca3f6e
todd-elvers <todd.elvers@gmail.com>
2019-09-09 19:50:06 -0500

    Move things that were missed in the Groovy -> Java conversion

--- src/main/java/te/htmltopdf/chrome/ChromePdfFileGenerator.java
+++ src/main/java/te/htmltopdf/chrome/ChromePdfFileGenerator.java
@@ -5,22 +5,21 @@ import com.github.kklisura.cdt.protocol.commands.Page;
 import com.github.kklisura.cdt.services.ChromeDevToolsService;
 import com.github.kklisura.cdt.services.types.ChromeTab;
 import io.vavr.control.Try;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.util.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import te.htmltopdf.TempFileGenerator;
 import te.htmltopdf.domain.ChromePdfOptions;
 import te.htmltopdf.domain.PdfFile;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.nio.file.Files;
-import java.util.Base64;
-
 class ChromePdfFileGenerator {
     private static final Logger log = LoggerFactory.getLogger(ChromePdfFileGenerator.class);
 
-    private ChromeServiceWrapper chromeServiceWrapper;
+    private ChromeServiceWrapper chromeServiceWrapper = new ChromeServiceWrapper();
     private TempFileGenerator tempFileGenerator;    // TODO: add use case for the temp file generation here
 
     PdfFile generate(ChromePdfOptions options, File htmlFile) {
@@ -32,8 +31,6 @@ class ChromePdfFileGenerator {
                 .get();
 
         try {
-//            chromeTab = chromeServiceWrapper.getChromeTab();
-//            devToolsService = chromeServiceWrapper.getChromeDevToolsService(chromeTab);
             devToolsService.getEmulation().setEmulatedMedia("print");
 
             Page page = devToolsService.getPage();
@@ -44,7 +41,7 @@ class ChromePdfFileGenerator {
                 try {
                     writeContentsToFile(pdfFileContents, rawPdfFile);
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    log.error("Failed to write contents to to PDF file.", e);
                 }
 
                 devToolsService.close();
@@ -55,8 +52,8 @@ class ChromePdfFileGenerator {
             if (devToolsService != null) {
                 devToolsService.waitUntilClosed();
             }
+
             chromeServiceWrapper.closeTab(chromeTab);
-            htmlFile.delete();
         }
         log.debug("PDF generation complete.");
         return new PdfFile(rawPdfFile);
