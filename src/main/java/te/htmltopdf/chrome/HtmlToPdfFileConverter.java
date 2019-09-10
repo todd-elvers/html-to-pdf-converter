2050238e5896efc7910b7e7d58c34a6bc31d956d
todd-elvers <todd.elvers@gmail.com>
2019-09-09 19:58:05 -0500

    Spiked out a functioning version of HTML -> PDF using Puppeteer

--- src/main/java/te/htmltopdf/chrome/HtmlToPdfFileConverter.java
+++ src/main/java/te/htmltopdf/chrome/HtmlToPdfFileConverter.java
@@ -1,33 +1,38 @@
 package te.htmltopdf.chrome;
 
+import java.io.File;
 import te.htmltopdf.domain.ChromePdfOptions;
 import te.htmltopdf.domain.PdfFile;
 
-import java.io.File;
-
 class HtmlToPdfFileConverter {
 
-    private ChromePdfFileGenerator chromePdfGenerator;
+    //TODO: Remove this hard coding
+    public static final String INPUT_FILE_PATH = "C:\\Users\\Todd\\Desktop\\big.html";
 
-    PdfFile convert(String html) {
+    private ChromePdfFileGenerator chromePdfGenerator = new ChromePdfFileGenerator();
+
+    // Works perfectly with HTML we haven't modified
+    // tl;dr having a method that simply takes a string of HTML is non-trivial
+    //TODO: Add a String equivalent of this once the rest of the logic is flushed out
+    PdfFile convert(File htmlContents) {
         ChromePdfOptions options = new ChromePdfOptions.Builder()
-                .setDisplayHeaderFooter(true)
-                .setPrintBackground(true)
-                .setScale(1d)
-                .setPaperWidth(8.5d)
-                .setPaperHeight(11d)
-                .setMarginTop(0.4d)
-                .setMarginBottom(0.4d)
-                .setMarginLeft(0.4d)
-                .setMarginRight(0.4d)
-                .setPageRanges("")
-                .setIgnoreInvalidPageRanges(false)
-                .setHeaderTemplate(" ")
-                .setFooterTemplate(" ")
-                .setPreferCSSPageSize(false)
-                .build();
+            .setDisplayHeaderFooter(true)
+            .setPrintBackground(true)
+            .setScale(1d)
+            .setPaperWidth(8.5d)
+            .setPaperHeight(11d)
+            .setMarginTop(0.4d)
+            .setMarginBottom(0.4d)
+            .setMarginLeft(0.4d)
+            .setMarginRight(0.4d)
+            .setPageRanges("")
+            .setIgnoreInvalidPageRanges(false)
+            .setHeaderTemplate(" ")
+            .setFooterTemplate(" ")
+            .setPreferCSSPageSize(false)
+            .build();
 
-
-        return chromePdfGenerator.generate(options, new File("some-file"));
+        return chromePdfGenerator.generate(options, htmlContents);
     }
+
 }
