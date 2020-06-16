1c8ba440d11b5c26c41408af777501bb3562e4ae
Todd Elvers <todd.elvers@gmail.com>
2020-06-15 22:49:11 -0500

    Removed dead code

--- src/main/java/te/htmltopdf/OutputStreamWritable.java
+++ src/main/java/te/htmltopdf/OutputStreamWritable.java
@@ -3,11 +3,6 @@ package te.htmltopdf;
 import java.io.IOException;
 import java.io.OutputStream;
 
-/**
- * Represents a PDF that can be written to an {@link OutputStream}.
- */
 public interface OutputStreamWritable {
-
     void write(OutputStream outputStream) throws IOException;
-
 }
